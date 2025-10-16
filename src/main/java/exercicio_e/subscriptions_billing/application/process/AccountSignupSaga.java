f2900bec69402ec3634ab3a1380052c5c2027d5a
lucasxf <lucasxferreira@gmail.com>
2025-10-16 08:54:47 -0300

    created acc signup saga

--- src/main/java/exercicio_e/subscriptions_billing/application/process/AccountSignupSaga.java
+++ src/main/java/exercicio_e/subscriptions_billing/application/process/AccountSignupSaga.java
@@ -1,13 +1,53 @@
 package exercicio_e.subscriptions_billing.application.process;
 
+import exercicio_e.subscriptions_billing.domain.account.event.AccountEvent.AccountCreated;
+import exercicio_e.subscriptions_billing.domain.subscription.event.TrialStarted;
+import exercicio_e.subscriptions_billing.domain.username.event.UsernameEvent.UsernameClaimed;
+import exercicio_e.subscriptions_billing.domain.username.event.UsernameEvent.UsernameReserved;
+import exercicio_e.subscriptions_billing.infrastructure.messaging.EventBus;
+import exercicio_e.subscriptions_billing.infrastructure.messaging.EventEnvelope;
+
 /**
  * @author lucas
  * @date 11/10/2025 10:25
  */
 public class AccountSignupSaga implements AutoCloseable {
 
+    private final EventBus eventBus;
+
+    public AccountSignupSaga(EventBus eventBus) {
+        this.eventBus = eventBus;
+        registerHandlers();
+    }
+
+    private void registerHandlers() {
+        final AutoCloseable reservedCloseable = eventBus.subscribe(UsernameReserved.class,
+                this::handleUsernameReserved);
+        final AutoCloseable createdCloseable = eventBus.subscribe(AccountCreated.class,
+                this::handleAccountCreated);
+        final AutoCloseable claimedCloseable = eventBus.subscribe(UsernameClaimed.class,
+                this::handleUsernameClaimed);
+        final AutoCloseable trialStartedCloseable = eventBus.subscribe(TrialStarted.class,
+                this::handleTrialStarted);
+
+    }
+
+    private void handleUsernameReserved(EventEnvelope<?> eventEnvelope) {
+    }
+
+    private void handleAccountCreated(EventEnvelope<?> eventEnvelope) {
+
+    }
+
+    private void handleUsernameClaimed(EventEnvelope<?> eventEnvelope) {
+
+    }
+
+    private void handleTrialStarted(EventEnvelope<?> eventEnvelope) { }
+
     @Override
     public void close() throws Exception {
+        // Unsubscribe from all events if needed
 
     }
 
